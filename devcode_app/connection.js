df853ba710705c55d4c310cb6982f60fe38884a4
DanzTim <dandi.temmu@hotmail.com>
2022-08-18 14:14:45 +0800

    changed to devcode

--- devcode_app/connection.js
+++ devcode_app/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 let mysql = require('mysql2/promise');
 
 let pool = mysql.createPool({
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
